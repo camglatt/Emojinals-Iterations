e7c6cf4a9600224331f807cd32af0b933150fa9e
camglatt <53193975+camglatt@users.noreply.github.com>
2019-07-26 11:38:19 -0400

    Add files via upload
    Iteration 3

--- Emojinals/ViewController.swift
+++ Emojinals/ViewController.swift
@@ -9,27 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-   
+     let emojis = ["🤠": "cowboy", "🏙": "city"]
+    
+    let customMessages = ["cowboy": ["y'all", "partner", "buckaroo","howdy"] , "city" : ["you guys", "friends", "buddy", "hey"] ]
+    
+    
     @IBAction func showMessage(sender: UIButton) {
+        let selectedEmotion = sender.titleLabel?.text
+      
         // in as much detail as possible
         // pseudo-code the things that need to happen
         // when a user clicks an emoji button
-        let alertController = UIAlertController(title: "If you're from the South, you say:", message: "Y'all", preferredStyle: UIAlertController.Style.alert)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[0]
+        let number = Int.random(in: 0 ... 3)
+        
+        let alertController = UIAlertController(title: "Where you're from, you may say...", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         
         alertController.addAction (UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
         
-       //  let emoji =
-    //     {"🤠";   "Y'all";
-       //     "🏙"; "You guys"
-    }
-
-
-            
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+      
+        
 }
+}
+
